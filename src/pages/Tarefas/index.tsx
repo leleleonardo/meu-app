458359e6710d78158943375a6461ff9f8b167f1b
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2024-04-17 17:36:04 -0300

    alterações home e tarefas

--- src/pages/Tarefas/index.tsx
+++ src/pages/Tarefas/index.tsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom"
+import styles from './tarefas.module.css'
 
 function Tarefas(){
     return(
         <>
-        <div>
+        <div className={styles.container}>
         <h1>Tarefas</h1>
         <Link to='/'> Voltar para Home </Link>
+        
+
+        </div>
+        <body className={styles.body}>
         <ul>
             <li> Tarefa 1</li>
             <li> Tarefa 2</li>
             <li> Tarefa 3</li>
             <li> Tarefa 4</li>
         </ul>
-
-        </div>
+        </body>
         </>
     )
 }
